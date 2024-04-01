aa23f1c536538d426d9a04387e07f3463033e4d3
hanif786 <hanifdostmuhammad8193@gmail.com>
2024-04-01 15:45:28 +0500

    push my todolist to github

---
+++ todolist/app.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition)
+    
+{
+    let addtask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what you  want to add your todos?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false"
+        }
+    ]);
+    todos.push(addtask.todo);
+    condition = addtask.addMore
+    console.log(todos);
+}
+
+
